8ae25486ee7911410054900d3de4d300e6ea1931
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-14 05:17:04 +0100

    feat: Project complete: was fun

--- error-handling.js
+++ error-handling.js
@@ -1,22 +1,48 @@
 // 1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
+ 
+	for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
   return sum;
 }
 
+
+//1.
+//tests
+try{
 let res = sum(null);
 console.log(res);
+} catch(error){
+if(error instanceof TypeError){
+console.log(`Wrong Type: ${error.message}`);
+} else{
+console.error(error.message)
+}
+}
 
+//2.
+function sayName(name){
+  if(typeof name!= "string"){
+   throw new TypeError("Invalid name! Must be a string!");
+  }
+
+	console.log(name);
+}
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here
+try{
+	sayName("Alex");
+	sayName(23);
+} catch(error){
+if(error instanceof TypeError){
+console.error(error.name + ": " + error.message);
+}
+}
 
-// 3.
+
+//3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
@@ -24,3 +50,16 @@ function greet(greeting) {
 
   console.log(greeting);
 }
+
+
+//3. tests
+try{
+greet("GOOD Morning fellas!!");
+greet(nowayjose);
+greet(null);
+}
+
+catch(error){
+console.error("Hello World")
+}
+
